2e864e3f28fd3a8059d88c749cb5ff0ed9c3a5b0
tmmr <tmmr007@gmail.com>
2018-07-19 17:48:59 -0400

    First Commit

---
+++ tests/test_deep.py
@@ -0,0 +1,135 @@
+import matplotlib
+import numpy as np
+
+matplotlib.use('Agg')
+import shap
+
+def test_tf_keras_mnist_cnn():
+    """ This is the basic mnist cnn example from keras.
+    """
+
+    try:
+        import tensorflow as tf
+        from tensorflow.python import keras
+        from tensorflow.python.keras.models import Sequential
+        from tensorflow.python.keras.layers import Dense, Dropout, Flatten, Activation
+        from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
+        from tensorflow.python.keras import backend as K
+    except Exception as e:
+        print("Skipping test_tf_keras_mnist_cnn!")
+        return
+    import shap
+
+    batch_size = 128
+    num_classes = 10
+    epochs = 1
+
+    # input image dimensions
+    img_rows, img_cols = 28, 28
+
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    model = Sequential()
+    model.add(Conv2D(8, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(Conv2D(16, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(32, activation='relu')) # 128
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
+    model.fit(x_train[:1000,:], y_train[:1000,:],
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test[:1000,:], y_test[:1000,:]))
+
+    # explain by passing the tensorflow inputs and outputs
+    np.random.seed(0)
+    inds = np.random.choice(x_train.shape[0], 10, replace=False)
+    e = shap.DeepExplainer((model.layers[0].input, model.layers[-1].input), x_train[inds,:,:])
+    shap_values = e.shap_values(x_test[:1])
+
+    sess = tf.keras.backend.get_session()
+    diff = sess.run(model.layers[-1].input, feed_dict={model.layers[0].input: x_test[:1]}) - \
+    sess.run(model.layers[-1].input, feed_dict={model.layers[0].input: x_train[inds,:,:]}).mean(0)
+
+    sums = np.array([shap_values[i].sum() for i in range(len(shap_values))])
+    d = np.abs(sums - diff).sum()
+    assert d / np.abs(diff).sum() < 0.001, "Sum of SHAP values does not match difference! %f" % d
+
+def test_keras_imdb_lstm():
+    """ Basic LSTM example using keras
+    """
+
+    try:
+        import keras
+        import numpy as np
+        import tensorflow as tf
+        from keras.datasets import imdb
+        from keras.models import Sequential
+        from keras.layers import Dense
+        from keras.layers import LSTM
+        from keras.layers.embeddings import Embedding
+        from keras.preprocessing import sequence
+    except Exception as e:
+        print("Skipping test_keras_imdb_lstm!")
+        return
+    import shap
+
+    # load the data from keras
+    np.random.seed(7)
+    max_features = 1000
+    (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=max_features)
+    X_train = sequence.pad_sequences(X_train, maxlen=100)
+    X_test = sequence.pad_sequences(X_test, maxlen=100)
+
+    # create the model. note that this is model is very small to make the test
+    # run quick and we don't care about accuracy here
+    mod = Sequential()
+    mod.add(Embedding(max_features, 8))
+    mod.add(LSTM(10, dropout=0.2, recurrent_dropout=0.2))
+    mod.add(Dense(1, activation='sigmoid'))
+    mod.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # select the background and test samples
+    inds = np.random.choice(X_train.shape[0], 3, replace=False)
+    background = X_train[inds]
+    testx = X_test[10:11]
+
+    # explain a prediction and make sure it sums to the difference between the average output
+    # over the background samples and the current output
+    e = shap.DeepExplainer((mod.layers[0].input, mod.layers[-1].input), background)
+    shap_values = e.shap_values(testx)
+    sums = np.array([shap_values[i].sum() for i in range(len(shap_values))])
+    sess = tf.keras.backend.get_session()
+    diff = sess.run(mod.layers[-1].input, feed_dict={mod.layers[0].input: testx})[0,:] - \
+        sess.run(mod.layers[-1].input, feed_dict={mod.layers[0].input: background}).mean(0)
+    assert np.allclose(sums, diff, atol=1e-06), "Sum of SHAP values does not match difference!"
